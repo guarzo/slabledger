cd184e3163fec7d00e25c1c65f42fc4d621bb31d
Guarzo <guarzo.eve@gmail.com>
2026-04-08 11:05:30 +0000

    fix: dashboard layout

--- web/src/react/pages/DashboardPage.tsx
+++ web/src/react/pages/DashboardPage.tsx
@@ -1,8 +1,6 @@
 import PokeballLoader from '../PokeballLoader';
-import { usePortfolioHealth, useWeeklyReview, useCapitalSummary, useCapitalTimeline } from '../queries/useCampaignQueries';
+import { usePortfolioHealth, useWeeklyReview, useCapitalSummary } from '../queries/useCampaignQueries';
 import HeroStatsBar from '../components/portfolio/HeroStatsBar';
-import CapitalTimelineChart from '../components/portfolio/CapitalTimelineChart';
-import CapitalExposurePanel from '../components/portfolio/CapitalExposurePanel';
 import WeeklyReviewSection from '../components/portfolio/WeeklyReviewSection';
 import WatchlistSection from '../components/watchlist/WatchlistSection';
 import AIAnalysisWidget from '../components/advisor/AIAnalysisWidget';
@@ -14,7 +12,6 @@ export default function DashboardPage() {
   const { data: healthData, isLoading: healthLoading } = usePortfolioHealth();
   const { data: weeklyReview } = useWeeklyReview();
   const { data: capitalData } = useCapitalSummary();
-  const { data: capitalTimeline } = useCapitalTimeline();
 
   if (healthLoading) {
     return (
@@ -33,18 +30,6 @@ export default function DashboardPage() {
 
       {/* Tier 1: Hero Stats Bar */}
       <HeroStatsBar health={healthData} capital={capitalData} />
-
-      {/* Capital Exposure + Timeline */}
-      {(capitalData || capitalTimeline) && (
-        <div className="mb-6">
-          <SectionErrorBoundary sectionName="Capital Exposure">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-              <CapitalExposurePanel capital={capitalData} />
-              {capitalTimeline && <CapitalTimelineChart data={capitalTimeline} />}
-            </div>
-          </SectionErrorBoundary>
-        </div>
-      )}
 
       {/* Weekly Review */}
       {weeklyReview && (
