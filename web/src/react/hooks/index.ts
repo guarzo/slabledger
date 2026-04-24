9ec54bf2727ac9a63146ae625042f7d8d722d98a
Guarzo <guarzo.eve@gmail.com>
2026-04-24 17:13:36 +0000

    chore: remove orphaned useRipple hook
    No longer used after Button rewrite to CSS Modules.

--- web/src/react/hooks/index.ts
+++ web/src/react/hooks/index.ts
@@ -6,8 +6,6 @@
 export { useLocalStorage } from './useLocalStorage';
 export { useDebounce } from './useDebounce';
 export { useMediaQuery } from './useMediaQuery';
-
-export { useRipple } from './useRipple';
 export { useForm } from './useForm';
 export { useCampaignDerived } from './useCampaignDerived';
 export { useSellSheet } from './useSellSheet';
