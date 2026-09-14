66c26310f395a0e1cc56aae1fa9fd73061a1fa11
Guarzo <guarzo.eve@gmail.com>
2026-09-14 18:47:17 -0400

    fix: remove inactive React plugin blocking ESLint 10

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -45,7 +44,6 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tseslint,
-      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -58,8 +56,6 @@ export default [
       '@typescript-eslint/no-explicit-any': 'warn',
 
       // React rules
-      'react/react-in-jsx-scope': 'off', // Not needed in React 17+
-      'react/prop-types': 'off', // Using TypeScript for prop validation
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
